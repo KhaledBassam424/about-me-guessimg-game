f76b82e3f2e0dbdf860b438b26d5eec50279ee53
KhaledBassam424 <khaleddardour427@gmail.com>
2021-02-13 03:25:08 +0200

    final update

--- js/app.js
+++ js/app.js
@@ -19,14 +19,14 @@ alert('you are about entering a guess game with us. Are you ready? lets go!');
 function games(){
 let favGame=prompt('do i like gaming');
 
-// The accepted answers should be yes or no
+
 favGame = favGame.toLowerCase();
 if(favGame==='yes' || favGame==='y'){
   alert('Sorry, That is wrong answer ');
-  console.log('wrong');
+  
 }else if(favGame==='no' || favGame==='n'){
     alert('you are right, congratualations');
-    console.log('right') ;
+  
     score=score+1 ;
 }
 }
@@ -35,8 +35,7 @@ function meals(){
 let favMeal=prompt('is my favourite meal Mansaf');
 favMeal=favMeal.toUpperCase();
 if((favMeal==='YES') || (favMeal==='Y')){
-    alert('you are wrong, i dont like Mansaf actually ');
-    console.log('wrong');
+    alert('you are wrong, i dont like Mansaf actually');
 }else if((favMeal==='NO') || (favMeal==='N')){ 
 alert('you are right, Im not a Mansaf fan');
 console.log('right');
@@ -49,11 +48,9 @@ let stars=prompt('do i like looking up at stars at night');
 stars=stars.toLowerCase();
 if((stars==='yes') || (stars==='y')){
     alert('gazing at stars is amazing thing, try to do it yourself');
-    console.log('right');
     score=score+1 ;
 }else if( (stars==='no') || (stars==='n')){
     alert('that is not correct');
-    console.log('wrong');
 }
 }
 
@@ -62,11 +59,10 @@ let dailyWalk= prompt('do i walk on a regular basis');
 dailyWalk=dailyWalk.toLowerCase();
 if(dailyWalk==='yes' || dailyWalk==='y'){
     alert('Health is the most important thing you have, you are right');
-    console.log('right');
     score=score+1 ;
 }else if(dailyWalk==='no' || dailyWalk==='n'){
     alert('you are wrong');
-    console.log('wrong');
+
 }
 }
 
@@ -75,10 +71,9 @@ let sugarFood= prompt('do i eat food which contain a lot of sugar');
 sugarFood=sugarFood.toLowerCase();
 if(sugarFood==='yes' || sugarFood==='y'){
     alert('Sorry, you are not right, a lot of sugar is harmful to your body');
-    console.log('wrong');
 }else if(sugarFood==='no' || sugarFood==='n'){
 alert('stay healthy, your answer is correct');
-console.log('correct');
+
 score=score+1 ;
 }
 }
@@ -89,7 +84,6 @@ let birthMonth=prompt('In which mothnth i was born, Enter a number');
 for(let i=1; i<=4; i++){
     if(parseInt(birthMonth) === 3){
         alert('smart boy');
-        console.log('correct');
         score=score+1;
         break;
     }else if((parseInt(birthMonth) >= 5) && (parseInt(birthMonth)<= 12)){
@@ -97,25 +91,23 @@ for(let i=1; i<=4; i++){
         birthMonth=prompt('try it agin');
     }else if((parseInt(birthMonth)<2) && (parseInt(birthMonth)>0)){
         alert('it is too low');
-        console.log('wrong');
+      
         birthMonth=prompt('try it again');
 }else if((parseInt(birthMonth)===2) || (parseInt(birthMonth)===4)){
     alert('you are close');
-    console.log('wrong');
+  
     birthMonth= prompt('try it again');
 }
 
 else{
     alert('out of scope input');
-    birthMonth=prompt('try it again');
-    console.log('wrong');
-}
+    birthMonth=prompt('try it again');    
 }
 if(parseInt(birthMonth)!==3){
     alert('3 is the correct answer. Thanks for trying guessing the right answer');
 }
 }
-
+}
 
 let colors= ['green' , 'pink' , 'aqua' , 'orange' , 'blue', 'white'];
 function myFavorite(){
@@ -125,7 +117,6 @@ for(let i=0; i<=5; i++){
     for(let j=0; j<=5 ; j++){
         if(favColor.toLowerCase() === colors[j]){
             alert('you are right');
-            console.log('right');
             score=score+1;
             found=true;
             break;
